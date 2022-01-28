37a1af7d4bec25cf303240345b62aa9d544ed68d
Damian Bustowski <dbustowski@protonmail.com>
2022-01-28 14:36:41 +0100

    Replaced ToogleSwitch with Tabs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import html
+from dash import html, dcc
 from dash.dependencies import Input, Output
 
 from jbi100_app.data import get_data
@@ -7,95 +7,69 @@ from jbi100_app.views.barplot import Barplot
 from jbi100_app.views.hexagon import MapViewHex
 from jbi100_app.views.menu import make_menu_layout
 
+# Create data
+df = get_data()
 
-def serve_chart_layout():
-    return html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout()
-            ),
+# Instantiate custom views
+barplot = Barplot("Barplot", df)
+mapViewHex = MapViewHex("Map-view")
 
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    barplot,
-                ],
-            ),
-        ],
-    )
+# Define layout
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        # Left column
+        html.Div(
+            id="left-column",
+            className="three columns",
+            children=make_menu_layout()
+        ),
+
+        # Right column
+        html.Div(
+            id="right-column",
+            className="nine columns",
+            children=[
+                dcc.Tabs(
+                    id='tab-aggregator',
+                    value='chart-view',
+                    children=[
+                        dcc.Tab(label='Chart View', value='chart-view'),
+                        dcc.Tab(label='Map View', value='map-view'),
+                    ]
+                ),
+                html.Div(id='tabs-content')
+            ],
+        ),
+    ],
+)
 
 
-def serve_map_layout():
-    return html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout(),
-            ),
+@app.callback(
+    Output(barplot.html_id, "figure"), [
+        Input("select-x-attribute-bar-1", "value"),
+        Input("select-x-attribute-bar-2", 'value'),
+        Input("date-picker-range", "start_date"),
+        Input("date-picker-range", 'end_date'),
+    ])
+def update_x(feature_x_1, feature_x_2, start_date, end_date):
+    # df = update_date(start_date, end_date)
+    # print(df['Date'].tolist())
+    #
+    # barplot.reload_df(df)
+    return barplot.update(feature_x_1, feature_x_2)
 
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    html.Div(
-                        style={"z-index": "-1", "background-color": "black"},
-                        id='map-subdiv',
-                        className="nine columns",
-                        children=mapViewHex
-                    ),
-                ],
-            ),
-        ],
-    )
+
+@app.callback(
+    Output('tabs-content', 'children'),
+    Input('tab-aggregator', 'value')
+)
+def update_view(tab):
+    if tab == 'chart-view':
+        return html.Div([barplot])
+    else:
+        return html.Div([mapViewHex])
 
 
 if __name__ == '__main__':
-    # Create data
-    df = get_data()
-
-    # Instantiate custom views
-    barplot = Barplot("Barplot", df)
-    mapViewHex = MapViewHex("Map-view")
-
-    app.layout = serve_chart_layout
-
-    @app.callback(
-        Output(barplot.html_id, "figure"), [
-            Input("select-x-attribute-bar-1", "value"),
-            Input("select-x-attribute-bar-2", 'value'),
-            Input("date-picker-range", "start_date"),
-            Input("date-picker-range", 'end_date'),
-        ])
-    def update_x(feature_x_1, feature_x_2, start_date, end_date):
-        # df = update_date(start_date, end_date)
-        # print(df['Date'].tolist())
-        #
-        # barplot.reload_df(df)
-        return barplot.update(feature_x_1, feature_x_2)
-
-
-    @app.callback(
-        Output('view-state-output', 'children'),
-        Input('view-switcher', 'value'),
-    )
-    def update_view(value):
-        if value is False:
-            state = 'Chart view'
-            app.layout = serve_chart_layout
-        else:
-            state = 'Map view'
-            app.layout = serve_map_layout
-
-        return state
-
     app.run_server(debug=False, dev_tools_ui=False, use_reloader=True)
