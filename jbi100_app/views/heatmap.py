ce2859de317299386f61a358d6eeac82625de3fd
Damian Bustowski <dbustowski@protonmail.com>
2022-01-30 19:00:12 +0100

    Removed temporary tooltip

--- jbi100_app/views/heatmap.py
+++ jbi100_app/views/heatmap.py
@@ -4,10 +4,10 @@ from dash import html, dcc
 
 
 class MapViewHeat(html.Div):
-    df = pd.read_csv('https://raw.githubusercontent.com/dbusn/JBI100-VIS/pydeck-testing/dataset_map.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/dbusn/JBI100-VIS/main/dataset_unique.csv')
 
     fig = go.Figure(go.Densitymapbox(lat=df.Latitude, lon=df.Longitude,
-                                     hovertext="hello", hoverinfo="text", radius=5))
+                                     hovertext="", hoverinfo="text", radius=5))
     fig.update_layout(mapbox_style="carto-darkmatter", mapbox_center_lat=51, mapbox_center_lon=0, width=1095,
                       height=650)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
