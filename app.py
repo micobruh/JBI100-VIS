cd6c2ff936bb93e6f1bbb25e8e2067c7bf8a9c06
micobruh <95045229+micobruh@users.noreply.github.com>
2022-02-04 11:17:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ from jbi100_app.views.menu import make_menu_layout
 df = get_data()
 
 # Instantiate custom views
-barplot = Barplot("barplot", df)
+barplot_0 = Barplot("barplot-1", df)
+barplot_1 = Barplot("barplot-2", df)
+barplot_2 = Barplot("barplot-3", df)
+barplot_3 = Barplot("barplot-4", df)
 mapViewHex = MapViewHex("hexmap")
 mapViewHeat = MapViewHeat("heatmap")
 
@@ -22,14 +25,14 @@ mapViewHeat = MapViewHeat("heatmap")
 app.layout = html.Div(
     id="app-container",
     children=[
-        # Left column
+        # Left column: Define menu in app
         html.Div(
             id="left-column",
             className="three columns",
             children=make_menu_layout(False)
         ),
 
-        # Right column
+        # Right column: Define 4 tabs/pages in app
         html.Div(
             id="right-column",
             className="nine columns",
@@ -38,7 +41,8 @@ app.layout = html.Div(
                     id='tab-aggregator',
                     value='chart-view',
                     children=[
-                        dcc.Tab(label='Chart View', value='chart-view'),
+                        dcc.Tab(label='Bar Chart View (With 2 Categorical Attributes)', value='chart-view'),
+                        dcc.Tab(label='Bar Chart View (With 3 Categorical Attributes)', value='chart-2-view'),
                         dcc.Tab(label='Hexmap View', value='hex-view'),
                         dcc.Tab(label='Heatmap View', value='heat-view')
                     ]
@@ -50,34 +54,77 @@ app.layout = html.Div(
     ],
 )
 
-
+# Generate barplot-1 in the 1st page of app
 @app.callback(
-    Output("barplot", "figure"),
+    Output("barplot-1", "figure"),
     Input("select-x-attribute-bar-1", "value"),
-    Input("select-x-attribute-bar-2", "value"),
-    Input("amount-or-percent", 'value'),
+    Input("select-x-attribute-bar-2", 'value'),
+    # Input("date-picker-range", "start_date"),
+    # Input("date-picker-range", "end_date"),
 )
-def update_x(feature_x_1, feature_x_2, type):
+def update_0(feature_x_1, feature_x_2):
     # df = update_date(start_date, end_date)
-    # print(df['Date'].tolist())
-    #
-    # barplot.reload_df(df)
-    return barplot.update(feature_x_1, feature_x_2, type)
+    # barplot_0.reload_df(df)
+    return barplot_0.update(feature_x_1, feature_x_2, "Amount")
 
+# Generate barplot-2 in the 1st page of app
+@app.callback(
+    Output("barplot-2", "figure"),
+    Input("select-x-attribute-bar-1", "value"),
+    Input("select-x-attribute-bar-2", 'value'),
+    # Input("date-picker-range", "start_date"),
+    # Input("date-picker-range", "end_date"),
+)
+def update_1(feature_x_1, feature_x_2):
+    # df = update_date(start_date, end_date)
+    # barplot_1.reload_df(df)
+    return barplot_1.update(feature_x_1, feature_x_2, "Percentage")
 
+# Generate barplot-3 in the 2nd page of app
+@app.callback(
+    Output("barplot-3", "figure"),
+    Input("select-x-attribute-bar-1", "value"),
+    Input("select-x-attribute-bar-2", 'value'),
+    Input("amount-or-percent", 'value'),
+    # Input("date-picker-range", "start_date"),
+    # Input("date-picker-range", "end_date"),
+)
+def update_2(feature_x_1, feature_x_2, type):
+    # df = update_date(start_date, end_date)
+    # barplot_2.reload_df(df)
+    return barplot_2.update(feature_x_1, feature_x_2, type)
+
+# Generate barplot-4 in the 2nd page of app
+@app.callback(
+    Output("barplot-4", "figure"),
+    Input("select-x-attribute-bar-1", "value"),
+    Input("select-x-attribute-bar-3", 'value'),
+    Input("amount-or-percent", 'value'),
+    # Input("date-picker-range", "start_date"),
+    # Input("date-picker-range", "end_date"),
+)
+def update_3(feature_x_1, feature_x_3, type):
+    # df = update_date(start_date, end_date)
+    # barplot_3.reload_df(df)
+    return barplot_3.update(feature_x_1, feature_x_3, type)
+
+# Generate menu on the left side of the app
 @app.callback(
     Output('tabs-content', 'children'),
     Output('left-column', 'children'),
-    Input('tab-aggregator', 'value'),
+    Input('tab-aggregator', 'value')
 )
+
+# Update menu based on the type of chart/view chosen
 def update_view(tab):
     if tab == 'chart-view':
-        return html.Div([barplot]), make_menu_layout(False)
+        return html.Div([barplot_0, barplot_1]), make_menu_layout(False)
+    elif tab == 'chart-2-view':
+        return html.Div([barplot_2, barplot_3]), make_menu_layout(False)
     elif tab == 'heat-view':
         return html.Div([mapViewHeat]), make_menu_layout(True)
     else:
         return html.Div([mapViewHex]), make_menu_layout(False)
-
 
 @app.callback(
     Output('heatmap', 'figure'),
@@ -93,3 +140,4 @@ def update_heatmap(attr, city):
 
 if __name__ == '__main__':
     app.run_server(debug=False, dev_tools_ui=False, use_reloader=True)
+
