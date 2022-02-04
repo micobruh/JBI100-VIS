39d470655147b765fb8d478e5c0d96bd6891a17d
micobruh <95045229+micobruh@users.noreply.github.com>
2022-02-04 11:14:04 +0100

    Update barplot.py

--- jbi100_app/views/barplot.py
+++ jbi100_app/views/barplot.py
@@ -11,35 +11,42 @@ class Barplot(html.Div):
         super().__init__(
             className="graph_card",
             children=[
-                html.H6("Barplot"),
+                html.H6(name),
                 dcc.Graph(id=self.html_id)
             ],
         )
 
     def update(self, feature_x_1, feature_x_2, type):
-        # TODO range of date
         self.fig = go.Figure()
 
+        # Create a list that stores all traces
         traces = []
+        # Normal bar chart when only 1 categorical attribute is given
         if feature_x_1 == feature_x_2:
-            temp = self.df[[feature_x_1]]
-            temp = temp[temp[feature_x_1] != '100000000']
-            temp = temp[temp[feature_x_1] != 100000000]
-            temp = temp.groupby(feature_x_1).size()
+            temp = self.df[[feature_x_2]]
+            # Filter off rows with unknown value
+            temp = temp[temp[feature_x_2]!='100000000']
+            temp = temp[temp[feature_x_2]!=100000000]
+            # Count number of accidents grouped by 1 attribute
+            temp = temp.groupby(feature_x_2).size()
             temp = temp.rename('Size').reset_index()
-            trace = go.Bar(name=feature_x_1, x=temp[feature_x_1], y=temp['Size'])
+            trace = go.Bar(name=feature_x_2, x=temp[feature_x_2], y=temp['Size'])
+            # Append all bars in horizontal direction
             traces.append(trace)
+        # Stacked bar chart when only 1 categorical attribute is given
         else:
             temp = self.df[[feature_x_1, feature_x_2]]
-            temp = temp[(temp[feature_x_1] != '100000000') & (temp[feature_x_2] != '100000000')]
-            temp = temp[(temp[feature_x_1] != 100000000) & (temp[feature_x_2] != 100000000)]
-            temp = temp.groupby([feature_x_1, feature_x_2]).size()
+            # Filter off rows with unknown value
+            temp = temp[(temp[feature_x_2]!='100000000')&(temp[feature_x_1]!='100000000')]
+            temp = temp[(temp[feature_x_2]!=100000000)&(temp[feature_x_1]!=100000000)]
+            # Count number of accidents grouped by 2 attributes
+            temp = temp.groupby([feature_x_2, feature_x_1]).size()
             temp = temp.rename('Size').reset_index()
-            for i in temp[feature_x_1].unique():
-                trace = go.Bar(name=str(i), x=temp[temp[feature_x_1] == i][feature_x_2],
-                               y=temp[temp[feature_x_1] == i]['Size'])
+            # Append all bars in horizontal direction first, and then in vertical direction to form stacked bar chart
+            for i in temp[feature_x_2].unique():
+                trace = go.Bar(name=str(i), x=temp[temp[feature_x_2]==i][feature_x_1], y=temp[temp[feature_x_2]==i]['Size'])
                 traces.append(trace)
-
+        
         for i in range(len(traces)):
             self.fig.add_trace(traces[i])
         self.fig.update_layout(
@@ -47,26 +54,29 @@ class Barplot(html.Div):
             xaxis_zeroline=False,
             barmode='stack',
         )
+        # Show the items in percentage based on attribute
+        # Otherwise show the items in the absolute amount
         if type == "Percentage":
             self.fig.update_layout(
                 barnorm='percent',
             )
 
+        # Add titles of x-axis, y-axis and legend
         self.fig.update_layout(
-            xaxis_title=feature_x_2.replace("_", " "),
+            xaxis_title=feature_x_1.replace("_", " "),
             yaxis_title="Number of Accidents",
-            legend_title=feature_x_1.replace("_", " "),
+            legend_title=feature_x_2.replace("_", " "),
         )
 
+        # Add the main title of the graph
         if feature_x_1 == feature_x_2:
             self.fig.update_layout(
-                title_text='Number of Accidents with ' + feature_x_1.replace("_", " "),
+                title_text='Number of Accidents with ' + feature_x_2.replace("_", " ") + ' (' + type + ')',
             )
         else:
             self.fig.update_layout(
-                title_text='Number of Accidents with ' + feature_x_2.replace("_",
-                                                                             " ") + ' Grouped by ' + feature_x_1.replace(
-                    "_", " "),
+                title_text='Number of Accidents with ' + feature_x_1.replace("_", " ") + 
+                ' Grouped by ' + feature_x_2.replace("_", " ") + ' (' + type + ')',
             )
 
         return self.fig
